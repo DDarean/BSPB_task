40eeb4100b0f0fd567fb73491e724d6763914fa9
Vladislav Gogolin <vladislav_gogolin@epam.com>
2022-09-07 12:36:10 +0300

    description added

--- stapp.py
+++ stapp.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write("PASS")
+st.header('Пример работы приложений')
+st.write('Детальное описание - на страницах заданий')
+st.markdown('#### Задание 1 - график')
+st.image('src/task1-chart.PNG')
+st.markdown('#### Задание 2 - метрики')
+st.image('src/task2-metrics.PNG', width=300)
+st.markdown('#### Задание 3 - таблица')
+st.image('src/task3-table.PNG', width=300)
