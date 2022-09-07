711299486221f4a815396db050587efbb66275ae
Vladislav Gogolin <vladislav_gogolin@epam.com>
2022-09-07 11:52:53 +0300

    task 3 final

--- pages/task_3_solution.py
+++ pages/task_3_solution.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-from solutions.task_3 import cashback_summary, process_csv
+from solutions.task_3 import Task3solver
 
 month = st.number_input(label='Payment start month', min_value=2, max_value=12,
                         step=1, value=3)
@@ -8,18 +7,25 @@ month = st.number_input(label='Payment start month', min_value=2, max_value=12,
 min_payment = st.number_input(label='Min payment', value=100000, min_value=0,
                               step=1000)
 
-cb_size = st.number_input(label='Cashback sum', value=1000, min_value=0, step=100)
+cb_size = st.number_input(label='Cashback sum', value=1000, min_value=0,
+                          step=100)
 
 path_to_file = st.text_input(label='Path to data file',
-                             value='../data/task3.csv')
+                             value='data/task3.csv')
 
 if st.button(label='calculate CB'):
-    processed_csv = process_csv(path_to_file, month, min_payment)
-    summary = cashback_summary(processed_csv, cb_size)
-    summary = summary.reset_index()
-    st.dataframe(summary)
+    solver = Task3solver(month=3, min_payment=100000, cb_size=1000)
+    solver.process_df(path_to_file)
+    solver.make_clients_table()
+    st.write('Ежемесячные траты клиентов (пример)')
+    table = solver.clients_table[solver.clients_table['id_client'] == 0].drop(
+        'year', axis=1)
+    st.dataframe(table.head(5).style.format(formatter='{:.2f}'))
+    solver.check_for_cashback()
+    summary_table = solver.create_summary_table()
+    st.dataframe(summary_table.style.format(formatter='{:.0f}'))
 
-    csv = summary.to_csv().encode('utf-8')
+    csv = summary_table.to_csv().encode('utf-8')
 
     st.download_button(
         label="Download data as CSV",
--- solutions/task_3.py
+++ solutions/task_3.py
@@ -1,36 +1,82 @@
 import pandas as pd
 
 
-def process_csv(df_path, payment_start_month_number=3, min_monthly_sum=100000):
-    if not 1 < payment_start_month_number < 12:
-        raise ValueError('Check month number')
+class Task3solver:
+    """
+    Task 3 solution.
+    Pipeline:
+        process .csv file -> make clients table with monthly total ->
+        check if cashback conditions are met -> summary table
 
-    df = pd.read_csv(df_path)
-    df = df[['id_client', 'day', 'tran_sum']]
+    Attributes:
+        month: first month for cashback payment
+        min_payment: minimum spending required for cashback
+        cb_size: monthly cashback payment
+    """
+    def __init__(self, month: int, min_payment: int, cb_size: int):
+        if not 1 < month < 12:
+            raise ValueError('Check month number')
+        self.month = month
+        self.min_payment = min_payment
+        self.cb_size = cb_size
+        self.data = None
+        self.clients_table = None
+        self.cashback_table = None
 
-    df['month'] = df['day'].apply(lambda x: pd.to_datetime(x).month)
-    df['year'] = df['day'].apply(lambda x: pd.to_datetime(x).year)
+    def process_df(self, df_path: str) -> None:
+        """
+        Converts .csv file to pandas dataframe with required index and columns
+        :param df_path: path to .csv file with training data
+        :return: None
+        """
+        self.data = pd.read_csv(df_path)
+        self.data = self.data[['id_client', 'day', 'tran_sum']]
 
-    grouped = df.groupby(['id_client', 'year', 'month']).sum()
-    grouped = grouped.reset_index()
+    def make_clients_table(self) -> None:
+        """
+        Create summary table with monthly total for each client
+        :return: None
+        """
+        clients_table = self.data[:]
+        clients_table['month'] = clients_table['day'].apply(
+            lambda x: pd.to_datetime(x).month)
+        clients_table['year'] = clients_table['day'].apply(
+            lambda x: pd.to_datetime(x).year)
 
-    grouped['previous'] = grouped.groupby(['id_client', 'year'])[
-        'tran_sum'].apply(
-        lambda x: (x.shift(1, fill_value=0) >= min_monthly_sum) & (x >= min_monthly_sum))
+        clients_table = clients_table.groupby(
+            ['id_client', 'year', 'month']).sum()
+        clients_table['tran_sum'] = clients_table['tran_sum'].round(2)
+        self.clients_table = clients_table.reset_index()
 
-    grouped['cashback'] = False
+    def check_for_cashback(self) -> None:
+        """
+        Check if cashback conditions are met and create monthly summary table
+        with cashback flag for each client
+        :return: None
+        """
+        cb_table = self.clients_table[:]
+        cb_table['previous'] = cb_table.groupby(['id_client', 'year'])[
+            'tran_sum'].apply(
+            lambda x: (x.shift(1, fill_value=0) >= self.min_payment) & (
+                        x >= self.min_payment))
+        cb_table['cashback'] = False
 
-    for i in range(1, len(grouped)):
-        if (grouped.loc[i, 'month'] >= payment_start_month_number) and \
-                (grouped.loc[i, 'previous']) and not (grouped.loc[i - 1, 'cashback']):
-            grouped.loc[i, 'cashback'] = True
-        else:
-            grouped.loc[i, 'cashback'] = False
+        for i in range(1, len(cb_table)):
+            if (cb_table.loc[i, 'month'] >= self.month) and \
+                    (cb_table.loc[i, 'previous']) and not (
+                    cb_table.loc[i - 1, 'cashback']):
+                cb_table.loc[i, 'cashback'] = True
+            else:
+                cb_table.loc[i, 'cashback'] = False
+        self.cashback_table = cb_table
 
-    return grouped[['id_client', 'year', 'month', 'tran_sum', 'cashback']]
-
-
-def cashback_summary(df, cashback_size=1000):
-    summary = df.groupby(['year', 'month']).sum()[['cashback']]
-    summary['cashback_rub'] = summary['cashback'] * cashback_size
-    return summary
+    def create_summary_table(self):
+        """
+        Calculate cashback cases and monthly total based on cashback size
+        :return: summary table with calculated total
+        """
+        data = self.cashback_table[:]
+        summary = data.groupby(['year', 'month']).sum()[['cashback']]
+        summary['cashback_rub'] = summary['cashback'] * self.cb_size
+        summary.columns = ['Количество клиентов', 'Итого выплаты, тыс. руб.']
+        return summary
